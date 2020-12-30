b42beca6d280d60628919e03df09837074fa2279
rootbu <mr.moussawalid@gmail.com>
2020-12-30 15:23:12 +0100

    header maquette terminé

---
+++ src/Entity/Candidat.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidatRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CandidatRepository::class)
+ */
+class Candidat
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $parti;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $wiki;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getParti(): ?string
+    {
+        return $this->parti;
+    }
+
+    public function setParti(?string $parti): self
+    {
+        $this->parti = $parti;
+
+        return $this;
+    }
+
+    public function getWiki(): ?string
+    {
+        return $this->wiki;
+    }
+
+    public function setWiki(?string $wiki): self
+    {
+        $this->wiki = $wiki;
+
+        return $this;
+    }
+}
