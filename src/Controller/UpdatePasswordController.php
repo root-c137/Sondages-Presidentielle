3f6f7e6c47063b8fc8c68f842f0fbc0018d0adb6
rootbu <mr.moussawalid@gmail.com>
2021-01-13 10:51:27 +0100

    update password..

--- src/Controller/UpdatePasswordController.php
+++ src/Controller/UpdatePasswordController.php
@@ -2,19 +2,66 @@
 
 namespace App\Controller;
 
+use App\Form\UpdatePasswordFormType;
+use App\Form\UpdateUserFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UpdatePasswordController extends AbstractController
 {
     /**
-     * @Route("/update/password", name="Updatepassword")
+     * @Route("/update/password", name="UpdatePasswordForm")
      */
     public function index(): Response
     {
-        return $this->render('update_password/index.html.twig', [
-            'controller_name' => 'UpdatePasswordController',
+        $Form = $this->createForm(UpdatePasswordFormType::class, $this->getUser(), [
+            'action' => $this->generateUrl('UpdatePassword'),
+            'method' => 'POST'
         ]);
+
+        return $this->render('update_password/index.html.twig', [
+            'Form' => $Form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/update/password/update", name="UpdatePassword")
+     */
+    public function UpdatePass(Request $Request,  UserPasswordEncoderInterface $Encoder): Response
+    {
+
+       $Form = $this->createForm(UpdatePasswordFormType::class);
+       $Form->handleRequest($Request);
+
+       if($Form->isSubmitted() && $Form->isValid() )
+       {
+           $OldPass = $Form->get('password')->getData();
+           if($Encoder->isPasswordValid($this->getUser(), $OldPass) )
+           {
+               $NewPass = $Form->get('new_password')->getData();
+               $NewPassH = $Encoder->encodePassword($this->getUser(), $NewPass);
+
+               $this->getUser()->setPassword($NewPassH);
+
+               $Doc = $this->getDoctrine()->getManager();
+               $Doc->flush();
+
+               $this->addFlash('Msg', 'Votre mot de passe à bien été modifier.');
+               return $this->redirectToRoute('UpdatePasswordForm');
+           }
+           else
+           {
+               $this->addFlash('Err', "Le mot de passe actuel n'est pas valide");
+               return $this->redirectToRoute('UpdatePasswordForm');
+           }
+       }
+       else
+       {
+           $this->addFlash('Err', "Les mots de passes ne correspondent pas..");
+           return $this->redirectToRoute('UpdatePasswordForm');
+       }
     }
 }
--- src/Form/UpdatePasswordFormType.php
+++ src/Form/UpdatePasswordFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,48 @@ class UpdatePasswordFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-            ->add('codepostal')
-            ->add('datenaissance')
-            ->add('vote')
+            ->add('password', PasswordType::class, [
+                'label' => 'Mot de passe actuel',
+                'attr' => [
+                    'placeholder' => 'mot de passe actuel',
+                    'class' => 'FormControl'
+                ],
+                'label_attr' => [
+                    'class' => 'FormLabel'
+                ]
+            ])
+            ->add('new_password', RepeatedType::class, [
+                'mapped' => false,
+                'type' => PasswordType::class,
+                'invalid_message' => "Les mots de passe ne sont pas identiques.",
+                'label' => 'Nouveau mot de passe',
+                'required' => true,
+                'first_options' => [
+                    'label' => 'Nouveau mot de passe',
+                    'attr' => [
+                        'class' => 'FormControl',
+                        'placeholder' => 'mot de passe..'
+                    ],
+                    'label_attr' => [
+                        'class' => 'FormLabel'
+                        ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmez votre nouveau mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Confirmez votre nouveau mot de passe',
+                        'class' => 'FormControl'
+                    ],
+                    'label_attr' => [
+                        'class' => 'FormLabel'
+                    ]]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Modifier',
+                'attr' => [
+                    'class' => 'GoModifier'
+                ]
+            ])
         ;
     }
 
